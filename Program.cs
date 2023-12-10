56942f9d5f82509c7ea4a135cccac754f764d980
Ruslan Hordeiuk <rusgord59@gmail.com>
2023-12-10 19:42:47 +0200

    Query6

--- Program.cs
+++ Program.cs
@@ -157,11 +157,18 @@ namespace Practice_Linq
             //Query 6: Вивести всі матчі останнього чемпіоната світу з футболу (FIFA World Cup), починаючи з чвертьфіналів (тобто останні 8 матчів).
             //Матчі мають відображатися від фіналу до чвертьфіналів (тобто у зворотній послідовності).
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games
+                .Where(game => game.Tournament == "FIFA World Cup")
+                .OrderByDescending(game => game.Date)
+                .Take(8);
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 6 ========================");
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
             // див. приклад як має бути виведено:
 
