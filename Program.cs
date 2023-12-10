280dfe374fcb59e1dcb90671fa2e757dd98f2446
Ruslan Hordeiuk <rusgord59@gmail.com>
2023-12-10 19:03:28 +0200

    Query2

--- Program.cs
+++ Program.cs
@@ -68,11 +68,17 @@ namespace Practice_Linq
         {
             //Query 2: Вивести Friendly матчі збірної Італії, які вона провела з 2020 року.  
 
-            var selectedGames = games; // Корегуємо запит !!!
+            var selectedGames = games
+                .Where(game => (game.Home_team == "Italy" || game.Away_team == "Italy") && game.Tournament == "Friendly" && game.Date.Year >= 2020)
+                .OrderBy(game => game.Date.Year);
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 2 ========================");
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
             // див. приклад як має бути виведено:
 
